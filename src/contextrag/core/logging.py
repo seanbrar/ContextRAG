6d5424ac827f3d64a165d93bf7023e54f763672c
Sean Brar <hello@seanbrar.com>
2026-01-25 20:02:47 -0800

    refactor: remove legacy ingest, routing, and core modules

--- src/contextrag/core/logging.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-
-def get_logger(name: str) -> logging.Logger:
-    logger = logging.getLogger(name)
-    if not logger.handlers:
-        logger.addHandler(logging.NullHandler())
-    return logger
